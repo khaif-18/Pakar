f353b9d34d1f76aef7f9ec0f556125edf278eaec
khaif <mkhalifah.ext@huawei.com>
2026-06-09 15:49:50 +0700

    [chore] setup vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  poweredByHeader: false,
   images: {
     remotePatterns: [],
+    formats: ["image/avif", "image/webp"],
+  },
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
   },
 };
 
